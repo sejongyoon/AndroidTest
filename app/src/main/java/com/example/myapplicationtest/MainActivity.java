778ea33413e868239ac9d5c290d7bfdf92c90886
sejoonyoon <yoon2033@gmail.com>
2019-03-21 19:50:57 +0900

    Please change to onCreate

--- app/src/main/java/com/example/myapplicationtest/MainActivity.java
+++ app/src/main/java/com/example/myapplicationtest/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("SJ", "onCreate #3");
+        Log.d("SJ", "onCreate");
     }
 }
